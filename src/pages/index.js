36e841c0299cd45a5ca5b207f83097485965c7fe
Higor Menezes <higormenezes1996@gmail.com>
2019-10-13 20:56:13 -0400

    Was created page routes

---
+++ src/pages/index.js
@@ -0,0 +1,3 @@
+export { default as CharacterCatalog } from './CharacterCatalog';
+export { default as CharacterDetail } from './CharacterDetail';
+export { default as CharacterEdit } from './CharacterEdit';
---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { CharacterCatalog, CharacterDetail, CharacterEdit } from './pages';
+
+const Router = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" exact component={CharacterCatalog} />
+      <Route path="/detail" component={CharacterDetail} />
+      <Route path="/edit" component={CharacterEdit} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Router;
