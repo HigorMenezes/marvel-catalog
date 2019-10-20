11a2882ed75d73aecb5f661a773de27aa95dc783
Higor Menezes <higormenezes1996@gmail.com>
2019-10-20 17:36:19 -0400

    tested edit button on character details

--- src/pages/CharacterDetails/containers/CharacterDetailsLoader/CharacterDetailsLoader.test.jsx
+++ src/pages/CharacterDetails/containers/CharacterDetailsLoader/CharacterDetailsLoader.test.jsx
@@ -1,8 +1,9 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent, waitForElement } from '@testing-library/react';
 import { ThemeProvider } from 'styled-components';
 import { ApolloProvider } from '@apollo/react-hooks';
+import { MockedProvider } from '@apollo/react-testing';
 import { MemoryRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { theme } from '../../../../styles';
@@ -10,26 +11,59 @@ import { marvelql } from '../../../../api';
 import { store } from '../../../../store';
 import CharacterDetailsLoader from './CharacterDetailsLoader';
 
+import { GET_CHARACTER_BY_ID, GET_SERIES_BY_CHARACTER_ID } from './queries';
+
+const charactersMock = [
+  {
+    request: {
+      query: GET_CHARACTER_BY_ID,
+      variables: { where: { id: 1017100 } },
+    },
+    result: () => {
+      return {
+        data: {
+          characters: [
+            {
+              id: '1017100',
+              name: 'A-Bomb (HAS)',
+              thumbnail:
+                'http://i.annihil.us/u/prod/marvel/i/mg/3/20/5232158de5b16.jpg',
+              description:
+                "Rick Jones has been Hulk's best bud since day one, but now he's more than a friend...he's a teammate! Transformed by a Gamma energy explosion, A-Bomb's thick, armored skin is just as strong and powerful as it is blue. And when he curls into action, he uses it like a giant bowling ball of destruction! ",
+              __typename: 'Character',
+            },
+          ],
+        },
+      };
+    },
+  },
+  {
+    request: {
+      query: GET_SERIES_BY_CHARACTER_ID,
+      variables: { offset: 3, limit: 0, where: { characters: [1017100] } },
+    },
+    result: () => {
+      return {
+        data: {
+          series: [
+            {
+              id: '17765',
+              title: 'FREE COMIC BOOK DAY 2013 1 (2013)',
+              thumbnail:
+                'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg',
+              description: null,
+              __typename: 'Series',
+            },
+          ],
+        },
+      };
+    },
+  },
+];
+
 describe('Verify CharacterDetailsLoader layout', () => {
   const config = {
-    loading: false,
-    character: {
-      id: '123',
-      name: 'name-test',
-      description: 'description-test',
-      thumbnail: 'thumbnail-test',
-    },
-    series: [
-      {
-        id: '321',
-        title: 'title-test',
-        thumbnail: 'thumbnail-test',
-        description: 'description-test',
-      },
-    ],
-    handleEditCharacter: () => {},
-    thereAreMoreSeries: true,
-    handleFetchMoreSeries: () => {},
+    characterId: 1017100,
   };
 
   it('should render CharacterDetailsRender', () => {
@@ -45,5 +79,25 @@ describe('Verify CharacterDetailsLoader layout', () => {
       </Provider>
     );
     expect(getByTestId('CharacterDetailsRender')).not.toBeNull();
+  });
+
+  it('should render CharacterDetailsRender', async () => {
+    const { getByTestId } = render(
+      <Provider store={store}>
+        <MockedProvider mocks={charactersMock} addTypename={false}>
+          <MemoryRouter initialEntries={['/details?character_id=1017100']}>
+            <ThemeProvider theme={theme}>
+              <CharacterDetailsLoader {...config} />
+            </ThemeProvider>
+          </MemoryRouter>
+        </MockedProvider>
+      </Provider>
+    );
+    const button = await waitForElement(() =>
+      getByTestId('CharacterEditButton')
+        .getElementsByTagName('button')
+        .item(0)
+    );
+    fireEvent.click(button);
   });
 });
