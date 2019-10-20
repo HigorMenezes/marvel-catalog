fcc53879ecb586aed4879fed3a931e5df80f3ac2
Higor Menezes <higormenezes1996@gmail.com>
2019-10-20 17:13:12 -0400

    tested click on card

--- src/pages/CharacterCatalog/containers/CharacterCatalogLoader/CharacterCatalogLoader.test.jsx
+++ src/pages/CharacterCatalog/containers/CharacterCatalogLoader/CharacterCatalogLoader.test.jsx
@@ -1,8 +1,9 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent, waitForElement } from '@testing-library/react';
 import { ThemeProvider } from 'styled-components';
 import { ApolloProvider } from '@apollo/react-hooks';
+import { MockedProvider } from '@apollo/react-testing';
 import { MemoryRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { theme } from '../../../../styles';
@@ -10,9 +11,35 @@ import { marvelql } from '../../../../api';
 import { store } from '../../../../store';
 import CharacterCatalogLoader from './CharacterCatalogLoader';
 
+import { GET_ALL_CHARACTERS } from './queries';
+
+const charactersMock = [
+  {
+    request: {
+      query: GET_ALL_CHARACTERS,
+      variables: { offset: 1, limit: 0 },
+    },
+    result: () => {
+      return {
+        data: {
+          characters: [
+            {
+              id: '1011334',
+              thumbnail:
+                'http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg',
+              name: '3-D Man',
+              __typename: 'Character',
+            },
+          ],
+        },
+      };
+    },
+  },
+];
+
 describe('Verify CharacterCatalogLoader layout', () => {
   const config = {
-    offset: 10,
+    offset: 1,
   };
 
   it('should render CharacterCardList layout', () => {
@@ -28,5 +55,26 @@ describe('Verify CharacterCatalogLoader layout', () => {
       </Provider>
     );
     expect(getByTestId('CharacterCardList')).not.toBeNull();
+  });
+
+  it('should click on card', async () => {
+    const { getByTestId } = render(
+      <Provider store={store}>
+        <MockedProvider mocks={charactersMock} addTypename={false}>
+          <MemoryRouter initialEntries={['/characters']}>
+            <ThemeProvider theme={theme}>
+              <CharacterCatalogLoader {...config} />
+            </ThemeProvider>
+          </MemoryRouter>
+        </MockedProvider>
+      </Provider>
+    );
+
+    const button = await waitForElement(() =>
+      getByTestId('CharacterCard')
+        .getElementsByClassName('card')
+        .item(0)
+    );
+    fireEvent.click(button);
   });
 });
--- src/pages/CharacterCatalogSearched/containers/CharacterCatalogSearchedLoader/CharacterCatalogSearchedLoader.test.jsx
+++ src/pages/CharacterCatalogSearched/containers/CharacterCatalogSearchedLoader/CharacterCatalogSearchedLoader.test.jsx
@@ -1,14 +1,41 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent, waitForElement } from '@testing-library/react';
 import { ThemeProvider } from 'styled-components';
 import { ApolloProvider } from '@apollo/react-hooks';
+import { MockedProvider } from '@apollo/react-testing';
 import { MemoryRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { theme } from '../../../../styles';
 import { marvelql } from '../../../../api';
 import { store } from '../../../../store';
 import CharacterCatalogSearchedLoader from './CharacterCatalogSearchedLoader';
+
+import { GET_ALL_CHARACTERS_BY_START_NAME } from './queries';
+
+const charactersMock = [
+  {
+    request: {
+      query: GET_ALL_CHARACTERS_BY_START_NAME,
+      variables: { offset: 10, limit: 0, where: { nameStartsWith: 'hulk' } },
+    },
+    result: () => {
+      return {
+        data: {
+          characters: [
+            {
+              id: '1009351',
+              thumbnail:
+                'http://i.annihil.us/u/prod/marvel/i/mg/5/a0/538615ca33ab0.jpg',
+              name: 'Hulk',
+              __typename: 'Character',
+            },
+          ],
+        },
+      };
+    },
+  },
+];
 
 describe('Verify CharacterCatalogSearchedLoader layout', () => {
   const config = {
@@ -29,5 +56,26 @@ describe('Verify CharacterCatalogSearchedLoader layout', () => {
       </Provider>
     );
     expect(getByTestId('CharacterCardList')).not.toBeNull();
+  });
+
+  it('should click on card', async () => {
+    const { getByTestId } = render(
+      <Provider store={store}>
+        <MockedProvider mocks={charactersMock} addTypename={false}>
+          <MemoryRouter initialEntries={['/characters']}>
+            <ThemeProvider theme={theme}>
+              <CharacterCatalogSearchedLoader {...config} />
+            </ThemeProvider>
+          </MemoryRouter>
+        </MockedProvider>
+      </Provider>
+    );
+
+    const button = await waitForElement(() =>
+      getByTestId('CharacterCard')
+        .getElementsByClassName('card')
+        .item(0)
+    );
+    fireEvent.click(button);
   });
 });
